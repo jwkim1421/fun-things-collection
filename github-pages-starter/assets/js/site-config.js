90de925766f2ae4fae18b340cc3e42c7d851e8a0
jwkim1421 <80744143+jwkim1421@users.noreply.github.com>
2026-05-09 14:05:38 +0900

    site url changed

--- github-pages-starter/assets/js/site-config.js
+++ github-pages-starter/assets/js/site-config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
   siteName: "쿠쿠열차",
-  siteUrl: "https://coocoo.co.kr/",
+  siteUrl: "https://coocooing.kro.kr/",
   kakaoJavaScriptKey: "f45989bb71514aece0519d2a250e0e7b",
   adsenseClient: "ca-pub-7367164333546101",
   adsenseSlot: "4548135151"
